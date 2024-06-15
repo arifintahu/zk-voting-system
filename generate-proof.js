41920dea2a2476f47e2df3571989d027bedbf551
Miftahul Arifin <miftahul97@gmail.com>
2024-06-15 20:27:40 +0700

    fix: generate proof

--- generate-proof.js
+++ generate-proof.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 
 async function generateProof() {
   const { proof, publicSignals } = await snarkjs.groth16.fullProve(
-    { vote: 1, nullifier: 12345 }, // Example input
+    { vote: 1, voterId: 12345 }, // Example input
     "./build/vote_js/vote.wasm",
     "./build/vote_0000.zkey"
   );
