d4152794940afe5ac5ffa39573952505354e97f7
Miftahul Arifin <miftahul97@gmail.com>
2024-06-18 22:08:57 +0700

    feat: add scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,18 @@
+async function main() {
+    const Verifier = await ethers.getContractFactory("Verifier");
+    const verifier = await Verifier.deploy();
+    await verifier.deployed();
+    console.log("Verifier deployed to:", verifier.address);
+
+    const VoteVerifier = await ethers.getContractFactory("VoteVerifier");
+    const voteVerifier = await VoteVerifier.deploy();
+    await voteVerifier.deployed();
+    console.log("VoteVerifier deployed to:", voteVerifier.address);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
