5edc1268277cbd67f65f814caed27625698a260f
Miftahul Arifin <miftahul97@gmail.com>
2024-06-22 20:03:23 +0700

    feat: add register voter

--- ignition/modules/VotingVerifier.js
+++ ignition/modules/VotingVerifier.js
@@ -3,5 +3,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 module.exports = buildModule("VotingVerifierModule", (m) => {
   const votingVerifier = m.contract("VotingVerifier", []);
 
+ m.call(votingVerifier, "registerVoter", ["0x28ea15261a7e81645f43de81ecadee00be84fbac"])
+
   return { votingVerifier };
 });
